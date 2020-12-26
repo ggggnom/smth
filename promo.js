bdca385329f7fb7e7f19e8d124cdbf2851b90855
Натаха Шестакова <55283126+ggggnom@users.noreply.github.com>
2020-12-26 09:20:48 +0300

    Add files via upload

---
+++ promo.js
@@ -0,0 +1,266 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" media="screen" href="assets/navbarinner.debug-9ae85843e749fe49bb54c73cdf05910ec0739eb9bc5449d08bf16a1e656f29dc.css" />
+<nav>
+  <div class="high_width">
+    <div class="graphiklogo">
+        <img alt="логотип" src="assets/logographik-349564f34b5489a27603e35ccb2174dc9fe5370f66341784d6ae49b3773ad458.svg" />
+    </div>
+    <div class="fontlogo">
+        <img alt="логотип" src="assets/bluelogo-f028f173db181b87170c831f5bfb4f581f88e46816264e4655e7ee0ad3ed74ce.svg" />
+    </div>
+
+    <div class="menu_info">
+      <div class="menu_info_th">
+        <a href="http://localhost:3000/about">о нас</a>
+        <a href="http://localhost:3000/posts">преимущества</a>
+        <a href="http://localhost:3000/posts/new">контакты</a>
+      </div>
+      <div class="registration">
+          <a href="http://localhost:3000/users/sign_in">Sign In</a>  <a href="http://localhost:3000/users/sign_up">Sign Up</a>
+      </div>
+      <div class="menul">
+        <img alt="логотип" src="assets/menu-ddaae461b2c8b04a99bdc9dafd81b3b45c031c25fe68b89db17881c981004fb7.svg" />
+      </div>
+    </div>
+  </div>
+
+  <div class="menu_mobile">
+    <div class="cancel">
+      <img alt="логотип" src="assets/clear-6fc0331ee487198eb2f15854cfb23f2936e57a8dc5fc5d583c2f314e71d06a1c.svg" />
+    </div>
+    <div class="menu_mobile_th">
+      <div class="">
+        <a href="http://localhost:3000/about">О нас</a>
+        <a href="http://localhost:3000/posts">Преимущества</a>
+        <a href="http://localhost:3000/posts/new">Контакты</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<body class="promo">
+
+<div class="wrapper">
+  <section class="begining">
+    <div class="begin">
+      <img alt="Логотип" src="assets/imgcover1-f358f8d8fa0bd8649cc659e1879ba028b2e278b56608038d7b60d9edee961c18.svg" />
+      <img alt="Логотип" src="assets/cover1-9e4d6815ebaaff14b7e02bc5d895b6a39d20d2fc3d55dc074c2b1bb5907c8a3e.svg" />
+      <div class="begin_text">
+        <h1>Найдутся любые&nbsp;игры</h1>
+        <p>**А&nbsp;если какая-то не&nbsp;найдется, мы&nbsp;поможем тебе сделать её&nbsp;самому</p>
+        <button type="button" name="button">Вступить</button>
+      </div>
+    </div>
+    <div class="about">
+      <p>CoroOne&nbsp;&mdash; медиа-сервис, созданный<br> специально для комьюнити геймеров
+и&nbsp;разработчиков. Мы&nbsp;верим в&nbsp;то, что именно обьединившись, любители видеоигр и&nbsp;гейм-дизайнеры смогут воплотить все самые смелые идеи и&nbsp;фантазии.</p>
+    </div>
+    <div class="advantages">
+      <div class="advantage">
+          <img alt="Логотип" src="assets/logographik-349564f34b5489a27603e35ccb2174dc9fe5370f66341784d6ae49b3773ad458.svg" />
+          <h3>Публикации</h3>
+          <p>Узнавайте актуальные новости, читайте статьи о&nbsp;жизни разработчиков, смотрите обзоры на&nbsp;новые и&nbsp;культовые игры</p>
+      </div>
+      <div class="advantage">
+          <img alt="Логотип" src="assets/thing2-fb1cc22ce381b7d337a2726d2cadb2ecb7d8fa28a3df8c6baab46bfc6053e4f9.svg" />
+          <h3>Oбщение</h3>
+          <p>Участвуйте в&nbsp;обсуждениях концепции визуала: предлагайте свои идеи и&nbsp;получайте фидбек от&nbsp;других пользователей</p>
+      </div>
+      <div class="advantage">
+          <img alt="Логотип" src="assets/thing3-1df9d8d5914d24dc84e53ad5f6a85f2d7713452e12cb2d7ca4306ada3d5d96fd.svg" />
+          <h3>Соревнование</h3>
+          <p>Отгадывайте загадки, делитесь прогрессом по&nbsp;своему проекту и&nbsp;получайте за&nbsp;это ачивки и&nbsp;баллы для продвижения профиля</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="defenition">
+    <h2>Почему CoroOne?</h2>
+    <div class="defenition_block">
+      <div class="defenition_text">
+        <div class="defenition_text_th">
+          <h4>Вежливость</h4>
+          <p>Модераторы CoroOne тщательно следят за&nbsp;тем, чтобы в&nbsp;обсуждениях появлялась только обьективная критика, а&nbsp;в&nbsp;случае конфликтов помогают решить разногласия</p>
+        </div>
+        <div class="defenition_text_th">
+          <h4>Вдохновение</h4>
+          <p>Наблюдайте за&nbsp;тем, как создается инди-игра в&nbsp;реальном времени. Учитесь и&nbsp;черпайте идеи.</p>
+        </div>
+        <div class="defenition_text_th">
+          <h4>Поддержка</h4>
+          <p>Забыли чит-код? Не&nbsp;получается докрутить концепцию? Задайте вопрос и&nbsp;вам обязательно помогут</p>
+        </div>
+        <div class="defenition_text_last">
+          <h4>Мотивация</h4>
+          <p>Участвуйте в&nbsp;рейтинге инди-проектов. Мы&nbsp;создаем еженедельный список самых активных разработчиков, чтобы помочь вам найти спонсоров.</p>
+        </div>
+      </div>
+      <div class="big_format">
+        <img alt="Лг" src="assets/defimage1920-fac13d5931a7bed293d34481c7b0f38b3e4b25766d538050d2082ff5f7a438a7.svg" />
+      </div>
+      <div class="small_format">
+          <img alt="Логотип" src="assets/defimage-927f0e3a95138568b25e95cb9cf4af298d6c94857a04041e52a083e65ccd45d3.svg" />
+      </div>
+
+
+    </div>
+  </section>
+
+
+  <section class="category">
+    <div class="circle"></div>
+    <img alt="Логотип" src="assets/defendimage-041cbfbed0f44d178a1522247d6efa8710ffb4746ba349d3c41cdba482cfd8b5.svg" />
+    <div class="category_13">
+      <div class="category_13_text">
+        <h4>На&nbsp;любой вкус и&nbsp;цвет</h4>
+        <p>Именно столько категорий статей вы&nbsp;сможете найти в&nbsp;CoroOne. Анонсы, релизы, чит-коды, обзоры и&nbsp;многое другое в&nbsp;одном месте.</p>
+      </div>
+      <div class="category_13_img">
+        <img alt="Логотип" src="assets/13-097c448e2828632f5bee6909653d204cab11912e4e866c88e6102b77b4c8862c.svg" />
+        <img alt="Логотип" src="assets/category-088ed060155d670ba7fa9ef87d1a2f4adc2ee2793939f1843640d1f51c6f2d79.svg" />
+      </div>
+    </div>
+    <div class="category_kinds">
+      <div class="first_part">
+        <div class="kind_of_category">
+          <p>аркады</p>
+        </div>
+        <div class="kind_of_category">
+          <p>головоломки</p>
+        </div>
+        <div class="kind_of_category">
+          <p>симуляторы</p>
+        </div>
+        <div class="kind_of_category">
+          <p>стратегии</p>
+        </div>
+      </div>
+      <div class="second_part">
+        <div class="kind_of_category">
+          <p>приключения</p>
+        </div>
+        <div class="kind_of_category">
+          <p>ролевые</p>
+        </div>
+        <div class="kind_of_category">
+          <p>спортивные</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="fichi_for_desctop">
+    <div class="fichi_for_desctop_img">
+      <div class="fichi_for_desctop_li">
+        <div class="fichi_for_desctop_li_text">
+          <p>Участвуйте в&nbsp;рейтинге инди-проектов. CoroOne формирует еженедельный список самых активных разработчиков, чтобы помочь вам найти спонсоров.</p>
+        </div>
+        <div class="fichi_for_desctop_li_text_s">
+          <p>Геймификация и&nbsp;ачивки прямо в&nbsp;интерфейсе. Отгадывай загадки, узнавай интересные факты из&nbsp;мира видео-игр, получай баллы.</p>
+        </div>
+        <div class="fichi_for_desctop_li_text">
+          <p>Участвуйте в&nbsp;рейтинге инди-проектов. CoroOne формирует еженедельный список самых активных разработчиков, чтобы помочь вам найти спонсоров.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="fichi_for_mobile">
+    <div class="first_fich">
+      <div class="first_fich_img">
+        <img alt="Логотип" src="assets/firstfich-a2f42b082207d2833466368ec83cf45c1750ceb3c19df10b01f620443efb8afe.svg" />
+      </div>
+      <div class="first_fich_text">
+        <p>Участвуйте в&nbsp;рейтинге инди-проектов. CoroOne формирует еженедельный список самых активных разработчиков, чтобы помочь вам найти спонсоров.</p>
+      </div>
+    </div>
+
+    <div class="second_fich">
+      <div class="second_fich_img">
+        <img alt="Логотип" src="assets/secondfich-97265f41b8e2fc68bbd96654b2733c0ce3e62a5e05f07df5aaafaeb6d45a61ca.svg" />
+      </div>
+      <div class="second_fich_text">
+        <p>Геймификация и&nbsp;ачивки прямо в&nbsp;интерфейсе. Отгадывай загадки, узнавай интересные факты из&nbsp;мира видео-игр, получай баллы.</p>
+      </div>
+    </div>
+
+    <div class="third_fich">
+      <div class="third_fich_img">
+        <img alt="Логотип" src="assets/thirdfich-170a90a7c2ec8db7d4b98450da399547c0c1ca13fbe9cd589aa5301abb4fe73b.svg" />
+      </div>
+      <div class="third_fich_text">
+        <p>Участвуйте в&nbsp;рейтинге инди-проектов. CoroOne формирует еженедельный список самых активных разработчиков, чтобы помочь вам найти спонсоров.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="sell">
+    <div class="sellback">
+      <img alt="Логотип" src="assets/sell_back-65a8754e6ea3e1660deddcc1b15161a962e8b5f7a4db581a310c0406263d9591.svg" />
+    </div>
+    <div class="sellback_info">
+      <div class="sellback_text">
+        <h3>Присоединяйся к&nbsp;нашему дружному коммьюнити</h3>
+        <p>Вступайете в&nbsp;наше сообщество на&nbsp;сайте или скачивайте наше приложение CoroOne в&nbsp;Google play и&nbsp;App Store</p>
+        <button class="sell_button" type="button" name="button">Вступить</button>
+      </div>
+      <div class="sellback_text_img">
+        <img alt="Логотип" src="assets/peopleimg-82e78cadefa928f455e2c2321eebda6437376e4b004b8a4ab14fb63f86894d3b.svg" />
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+ </div>
+
+</body>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" media="screen" href="assets/footerinner.debug-c7956ad5fd1565854e7cb856ab488f4495a858ef2ab1549e197c780381c3bfd5.css" />
+
+<footer>
+  <div class="footer_menu">
+    <a href="http://localhost:3000/about">О нас</a>
+    <a href="http://localhost:3000/posts">Фичи</a>
+    <a href="http://localhost:3000/posts/new">Разработчикам</a>
+  </div>
+  <div class="footer_info">
+    <div class="footer_help">
+      <p>Остались вопросы? Пишите нам в&nbsp;телеграм:</p>
+      <p>+7 933 676 56 16</p>
+    </div>
+    <div class="footer_sites">
+       <p>Добавляетесь к&nbsp;нам в&nbsp;соц. сети! Будет много интересного</p>
+       <div class="footer_sites_th">
+          <img alt="Логотип" src="assets/inst-c628c0504f7826a190ef02f122c21c25e38cb81894aa9cf61ba3e772722346c6.svg" />
+          <img alt="Логотип" src="assets/vk-66704edfadae06b846b84d069e8f0c113e7bf99b57f0e40ea21f6952e0c8d1ec.svg" />
+          <img alt="Логотип" src="assets/youtube-bcc8c8d8f48ddaa63f38fcf01468d83ddc088f9fcbda8bf79ae4d31aac8a7398.svg" />
+          <img alt="Логотип" src="assets/twitter-fa2a508fd1534c102e5a9f70a31153e63404943b377cd9ae0d456909faf36fdb.svg" />
+       </div>
+    </div>
+  </div>
+  <div class="added_info">
+    <div class="small_logo">
+      <img alt="Логотип" src="assets/graylogo-6c4489f8ebd154f1f66624009a4b2b7e922211214f9561d03195e4b28b967e0a.svg" />
+    </div>
+    <div class="big_logo">
+        <img alt="Логотип" src="assets/big_graylogo-0b069f4c131ca6ab081923bba7bcd95d796a0307337616b6e66f220dc039dc44.svg" />
+    </div>
+    <div class="added_info_added">
+       <p>CoroOne</p>
+       <p>2020</p>
+    </div>
+  </div>
+
+</footer>
+
+
+<a href="http://localhost:3000/users">Пользователи</a>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.1.0/anime.min.js"></script>
+<script src="promo.js"></script>
